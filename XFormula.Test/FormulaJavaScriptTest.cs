9637a89d5dac230ba9d0a10deb9cf53148a84072
西奥笑 <w800098@qq.com>
2022-04-11 16:06:06 +0800

    增加解析为 JavaScript 可执行表达式逻辑

---
+++ XFormula.Test/FormulaJavaScriptTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Microsoft.ClearScript.V8;
+using XFormula.Parser;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace XFormula.Test;
+
+public class FormulaJavaScriptTest
+{
+    private readonly ITestOutputHelper _console;
+    private readonly V8ScriptEngine _engine = new();
+
+    public FormulaJavaScriptTest(ITestOutputHelper console)
+    {
+        _console = console;
+        _engine.ExecuteCommand("var v = { 'AA101': 15, 'CF711': 20, 'DD365': 61 }");
+    }
+
+    [Fact]
+    public void Test()
+    {
+        Validate("1 + 5 + 3 / 0 + 1");
+
+        Validate("(3.5 + 3.5 * 2 + 3.5) / 12 + 1");
+
+        Validate("ROUND((3.5 + 3.5 * 2 + 3.5) / 12 + 1, 2)");
+
+        Validate("ROUND(ABS(-12.365),2)");
+
+        Validate("AA101 + CF711 * 2 + DD365");
+
+        Validate("(AA101 + CF711 * 2 + DD365) / 12 + 1");
+
+        Validate("(((2+1)))");
+    }
+
+    private void Validate(string formula)
+    {
+        _console.WriteLine("公式：" + formula);
+        var formulaScript = FormulaJavaScriptVisitor.Parse(formula);
+        _console.WriteLine("包含指标：" + string.Join(",", formulaScript.SubCodes));
+        _console.WriteLine("解析为：" + formulaScript.Script);
+        _console.WriteLine("执行结果：" + _engine.ExecuteCommand($"eval(\"{formulaScript.Script}\")"));
+        _console.WriteLine("");
+    }
+}
---
+++ XFormula/Parser/FormulaJavaScriptVisitor.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using Antlr4.Runtime;
+using XFormula.Antlr4Gen;
+
+namespace XFormula.Parser;
+
+public class FormulaJavaScriptVisitor : FormulaBaseVisitor<IEnumerable<string>>
+{
+    /// <summary>
+    /// 子级指标编码
+    /// </summary>
+    public HashSet<string> SubCodes { get; } = new();
+
+    /// <summary>
+    /// 默认值
+    /// </summary>
+    private const string DEFAULT_VALUE = "0";
+
+    public override IEnumerable<string> VisitAtom(FormulaParser.AtomContext context)
+    {
+        switch (context.Start.Type)
+        {
+            case FormulaParser.NUMBER:
+                yield return context.NUMBER().GetText();
+                break;
+            case FormulaParser.VARIABLE:
+                yield return "v['";
+                var code = context.VARIABLE().GetText();
+                SubCodes.Add(code);
+                yield return code;
+                yield return "']";
+                break;
+            case FormulaParser.MINUS:
+                yield return "-";
+                foreach (var value in Visit(context.atom()))
+                    yield return value;
+                break;
+            default:
+                yield return DEFAULT_VALUE;
+                break;
+        }
+    }
+
+    public override IEnumerable<string> VisitParens(FormulaParser.ParensContext context)
+    {
+        return Visit(context.expr());
+    }
+
+    #region 运算符
+    public override IEnumerable<string> VisitOpFirst(FormulaParser.OpFirstContext context)
+    {
+        return VisitOperate(context.op, context);
+    }
+
+    public override IEnumerable<string> VisitOpSecond(FormulaParser.OpSecondContext context)
+    {
+        return VisitOperate(context.op, context);
+    }
+
+    [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+    private IEnumerable<string> VisitOperate(IToken op, FormulaParser.ExprContext context)
+    {
+        var left = Visit(context.GetRuleContext<FormulaParser.ExprContext>(0));
+        var right = Visit(context.GetRuleContext<FormulaParser.ExprContext>(1));
+
+        if (op.Type != FormulaParser.DIV)
+        {
+            foreach (var value in left)
+                yield return value;
+
+            yield return op.Text;
+
+            foreach (var value in right)
+                yield return value;
+        }
+        else
+        {
+            // 除法需要对除零时进行特殊处理，避免报错 
+            // ( right == 0 ? 0 : left / right )
+            yield return "(";
+            foreach (var value in right)
+                yield return value;
+
+            yield return "==0?0:";
+
+            foreach (var value in left)
+                yield return value;
+
+            yield return "/";
+
+            foreach (var value in right)
+                yield return value;
+
+            yield return ")";
+        }
+    }
+    #endregion
+
+    #region 函数
+    public override IEnumerable<string> VisitFunction(FormulaParser.FunctionContext context)
+    {
+        var func = context.func();
+        return func.funcName().Start.Type switch
+        {
+            FormulaParser.ABS => VisitAbs(func),
+            FormulaParser.ROUND => VisitRound(func),
+            _ => throw new ArgumentOutOfRangeException("暂不支持函数 " + func.funcName().GetText())
+        };
+    }
+
+    private IEnumerable<string> VisitAbs(FormulaParser.FuncContext context)
+    {
+        // Math.abs( value )
+        var arg = Visit(context.expr(0));
+
+        yield return "Math.abs(";
+
+        foreach (var value in arg)
+            yield return value;
+
+        yield return ")";
+    }
+
+    private IEnumerable<string> VisitRound(FormulaParser.FuncContext context)
+    {
+        var exprContexts = context.expr();
+
+        // Number( ( value ).toFixed( decimal ) )
+
+        yield return "Number((";
+
+        var valueExpr = Visit(exprContexts[0]);
+        foreach (var value in valueExpr)
+            yield return value;
+        yield return ").toFixed(";
+
+        if (exprContexts.Length <= 1)
+            yield return "0";
+        else
+        {
+            foreach (var value in Visit(exprContexts[1]))
+                yield return value;
+        }
+
+        yield return "))";
+    }
+    #endregion
+
+    /// <summary>
+    /// 解析公式
+    /// </summary>
+    public static FormulaScript Parse(string formula)
+    {
+        var errorOutput = new StringWriter();
+        var lexer = new FormulaLexer(CharStreams.fromString(formula),
+            new StringWriter(), errorOutput);
+        var tokens = new CommonTokenStream(lexer);
+        var parser = new FormulaParser(tokens, new StringWriter(), errorOutput);
+        var tree = parser.prog();
+        var errMsg = errorOutput.ToString();
+        if (errMsg.IsNotEmpty())
+            throw new SyntaxErrorException($"公式：{formula} 错误：{errMsg}");
+
+        var visitor = new FormulaJavaScriptVisitor();
+        var expr = visitor.Visit(tree);
+
+        var result = new FormulaScript { SubCodes = visitor.SubCodes };
+        if (expr != null)
+        {
+            var script = string.Concat(expr);
+            if (script.IsNotEmpty())
+                result.Script = script;
+        }
+        result.Script ??= DEFAULT_VALUE;
+        return result;
+    }
+
+    public class FormulaScript
+    {
+        public string Script { get; set; }
+        public HashSet<string> SubCodes { get; set; }
+    }
+}
